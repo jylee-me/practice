758d4aa38fede133dca3788e9d8b851081cb63dd
jylee-me <jylee9432@gmail.com>
2022-07-01 12:12:26 +0900

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*
+public class Main {
+    public static void main (String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+*/
+
+
+public class Main {
+    public static void main (String[] args){
+        System.out.print("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|\n");
+    }
+}
+
+
